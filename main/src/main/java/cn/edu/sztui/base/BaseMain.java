10e52fd559a0070e2d27afb082e14f230d7819c4
ZZQ323 <2208051122@qq.com>
2025-11-30 01:25:53 +0800

    教务系统登录

---
+++ main/src/main/java/cn/edu/sztui/base/BaseMain.java
@@ -0,0 +1,18 @@
+package cn.edu.sztui.base;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = "cn.edu.sztui")
+public class BaseMain {
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(BaseMain.class, args);
+            log.info("启动完成!");
+        } catch (Exception e) {
+            log.error("出现问题！" + e.getMessage());
+        }
+    }
+}
